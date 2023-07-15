22c6dfdbaa22f734a86f66d590ef1b00a5cdd95e
Xaypanya Phongsa <dackogan@gmail.com>
2023-07-16 02:07:51 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 
 export default function App() {
+
+  const [count, setCount] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={()=> setCount(count+1)}>
+        <View style={styles.nestedContainer}>
+          <Text style={styles.textStyle}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <Text style={{marginTop: 50, fontSize: 20}}>current Number: {count}</Text>
     </View>
-  );
+  )
 }
+
+const area = 200
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'coral',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  nestedContainer: {
+    width: area,
+    height: area,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00ffff',
+    borderRadius: 20
+  },
+  textStyle: {
+    fontSize: 50,
+    fontWeight: 800
+  }
+})
